12965fd834286ffa51d4f933fadfbc86a2b48682
Rudra Narayan Chaturvedi <killerdeath430@gmail.com>
2026-08-10 09:53:43 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& a) {
+        stack <int>st;
+        int n =a.size();
+        vector<int>ans(n,0);
+        for (int i=n-1;i>=0;i--){
+           
+            while(!st.empty()&&a[st.top()]<=a[i]){
+               
+                st.pop();
+            }
+ 
+              if(!st.empty()) ans[i]=st.top()-i;
+                st.push(i);
+       
+
+
+        }
+return ans;
+    }
+};
